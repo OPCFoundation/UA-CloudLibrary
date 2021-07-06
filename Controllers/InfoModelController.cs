8c2221cfd31b18469d5aa51e5fbb2602f267074c
barnstee <erichb@microsoft.com>
2021-07-06 13:45:24 +0200

    1. Updated Nugets to latest ones. 2. Added OPCF UA stack 3. Added parsing of uploaded nodeset file.

--- Controllers/InfoModelController.cs
+++ Controllers/InfoModelController.cs
@@ -4,7 +4,9 @@ namespace UA_CloudLibrary.Controllers
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
+    using Opc.Ua.Export;
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Threading.Tasks;
     using UA_CloudLibrary.Interfaces;
@@ -54,11 +56,86 @@ namespace UA_CloudLibrary.Controllers
             if (newFileHandle != string.Empty)
             {
                 // add a record of the new file to the index database, and get back the database ID for the new nodeset
+                List<string> namespaces = new List<string>();
+                using (Stream stream = new FileStream(newFileHandle, FileMode.Open))
+                {
+                    UANodeSet nodeSet = UANodeSet.Read(stream);
+                    foreach (string ns in nodeSet.NamespaceUris)
+                    {
+                        if (!namespaces.Contains(ns))
+                        {
+                            namespaces.Add(ns);
+                        }
+                    }
+
+                    foreach (UANode uaNode in nodeSet.Items)
+                    {
+                        UAVariable variable = uaNode as UAVariable;
+                        if (variable != null)
+                        {
+                           // skip over variables
+                            continue;
+                        }
+
+                        UAMethod method = uaNode as UAMethod;
+                        if (method != null)
+                        {
+                            // skip over methods
+                            continue;
+                        }
+
+                        UAObject uaObject = uaNode as UAObject;
+                        if (uaObject != null)
+                        {
+                            // skip over objects
+                            continue;
+                        }
+
+                        UAView view = uaNode as UAView;
+                        if (view != null)
+                        {
+                            // skip over views
+                            continue;
+                        }
+
+                        UAVariableType variableType = uaNode as UAVariableType;
+                        if (variableType != null)
+                        {
+                            // TODO: add variable types to database
+                            continue;
+                        }
+
+                        UADataType dataType = uaNode as UADataType;
+                        if (dataType != null)
+                        {
+                            // TODO: add data types to database
+                            continue;
+                        }
+
+                        UAReferenceType referenceType = uaNode as UAReferenceType;
+                        if (referenceType != null)
+                        {
+                            // TODO: add reference types to database
+                            continue;
+                        }
+
+                        UAObjectType objectType = uaNode as UAObjectType;
+                        if (objectType != null)
+                        {
+                            // TODO: add object types to database
+                            continue;
+                        }
+
+                        throw new ArgumentException("Unknown UA node detected!");
+                    }
+                }
+
                 int newID = await _database.AddNodeSetToDatabaseAsync(newFileHandle);
 
                 AddressSpace result = new AddressSpace();
                 result.ID = newID.ToString();
                 result.Nodeset.AddressSpaceID = newFileHandle;
+
                 // TODO: insert/gather other metadata
 
                 return result;
