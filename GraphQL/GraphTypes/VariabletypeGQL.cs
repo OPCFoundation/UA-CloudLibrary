bbd5ec7a97fe63681cf1dfa34c1ce74b9a0f682a
Leon Granzow <l.granzow@inray.de>
2021-09-21 14:25:51 +0200

    Created their GraphQL type classes

---
+++ GraphQL/GraphTypes/VariabletypeGQL.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UA_CloudLibrary.DbContextModels;
+
+namespace UA_CloudLibrary.GraphQL.GraphTypes
+{
+    public class VariabletypeGQL : EfObjectGraphType<AppDbContext, Variabletype>
+    {
+        public VariabletypeGQL(IEfGraphQLService<AppDbContext> service) : base(service)
+        {
+            AutoMap();
+        }
+    }
+}
