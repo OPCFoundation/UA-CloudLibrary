6cf5a68bea639e2d095d6e1d5d1b15c746bc7bc1
Leon Granzow <l.granzow@inray.de>
2021-12-17 13:50:16 +0100

    Use client library in sample

--- SampleConsoleClient/Program.cs
+++ SampleConsoleClient/Program.cs
@@ -33,9 +33,12 @@ namespace SampleConsoleClient
     using GraphQL.Client.Http;
     using GraphQL.Client.Serializer.Newtonsoft;
     using System;
+    using System.Collections.Generic;
     using System.Net;
     using System.Text;
     using System.Text.Json;
+    using UACloudLibClientLibrary;
+    using UACloudLibClientLibrary.Models;
 
     class Program
     {
@@ -53,6 +56,8 @@ namespace SampleConsoleClient
             TestGraphQLInterface(args);
 
             TestRESTInterface(args);
+
+            TestClientLibrary(args);
 
             Console.WriteLine();
             Console.WriteLine("Done!");
@@ -138,6 +143,44 @@ namespace SampleConsoleClient
             Console.WriteLine("For sample code to test /infomodel/upload, see https://github.com/digitaltwinconsortium/UANodesetWebViewer/blob/main/Applications/Controllers/UACL.cs");
 
             webClient.Dispose();
+        }
+
+        private static void TestClientLibrary(string[] args)
+        {
+            Console.WriteLine("\n\nTesting the client library");
+
+            UACloudLibClient client = new UACloudLibClient(args[0], args[1], args[2]);
+
+            Console.WriteLine("\nTesting object query");
+            List<ObjectResult> test = client.GetObjectTypes().GetAwaiter().GetResult();
+            foreach(ObjectResult result in test)
+            {
+                Console.WriteLine($"{result.ID}, {result.Namespace}, {result.Browsename}, {result.Value}");
+            }
+
+            Console.WriteLine("\nTesting metadata query");
+            List<MetadataResult> metadatas = client.GetMetadata().GetAwaiter().GetResult();
+            foreach(MetadataResult metadata in metadatas)
+            {
+                Console.WriteLine($"{metadata.ID}, {metadata.Name}, {metadata.Value}");
+            }
+
+            Console.WriteLine("\nTesting query and convertion of metadata");
+            List<AddressSpace> finalResult = client.GetCombinedResult().GetAwaiter().GetResult();
+            foreach(AddressSpace result in finalResult)
+            {
+                Console.WriteLine($"{result.Title} by {result.Contributor.Name} last update on {result.LastModification}");
+            }
+
+            if(finalResult.Count > 0)
+            {
+                Console.WriteLine("Testing download of nodeset");
+                AddressSpace result = client.DownloadNodeset(finalResult[0].ID.ToString()).GetAwaiter().GetResult();
+                if (!string.IsNullOrEmpty(result.Nodeset.NodesetXml))
+                {
+                    Console.WriteLine("Nodeset Downloaded");
+                }
+            }
         }
     }
 }
