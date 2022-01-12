918299adfb3401ba8b7ed3ca087e2580b95541d8
Leon Granzow <l.granzow@inray.de>
2022-01-12 17:01:40 +0100

    added graphql controller

---
+++ UACloudLibraryServer/GraphQL/GraphQLController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json.Linq;
+using GraphQL.EntityFramework;
+
+namespace UA_CloudLibrary.GraphQL
+{
+    
+
+    #region GraphQlController
+    [Route("[controller]")]
+    [ApiController]
+    public class GraphQlController :
+        Controller
+    {
+#nullable enable
+        IDocumentExecuter executer;
+        ISchema schema;
+        static DocumentWriter writer = new DocumentWriter(true);
+
+        public GraphQlController(ISchema schema, IDocumentExecuter executer)
+        {
+            this.schema = schema;
+            this.executer = executer;
+        }
+
+        [HttpPost]
+        public Task Post(
+            [BindRequired, FromBody] PostBody body,
+            CancellationToken cancellation)
+        {
+            return Execute(body.Query, body.OperationName, body.Variables, cancellation);
+        }
+
+        public class PostBody
+        {
+            public string? OperationName { get; set; }
+            public string Query { get; set; }
+            public JObject? Variables { get; set; }
+        }
+
+        [HttpGet]
+        public Task Get(
+            [FromQuery] string query,
+            [FromQuery] string? variables,
+            [FromQuery] string? operationName,
+            CancellationToken cancellation)
+        {
+            var jObject = ParseVariables(variables);
+            return Execute(query, operationName, jObject, cancellation);
+        }
+
+        async Task Execute(string query,
+            string? operationName,
+            JObject? variables,
+            CancellationToken cancellation)
+        {
+            ExecutionOptions options = new ExecutionOptions()
+            {
+                Schema = schema,
+                Query = query,
+                OperationName = operationName,
+                Inputs = variables?.ToInputs(),
+                CancellationToken = cancellation,
+#if (DEBUG)
+                ThrowOnUnhandledException = true,
+                EnableMetrics = true,
+#endif
+            };
+            var executeAsync = await executer.ExecuteAsync(options);
+
+            await writer.WriteAsync(Response.Body, executeAsync, cancellation);
+        }
+
+        static JObject? ParseVariables(string? variables)
+        {
+            if (variables == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                return JObject.Parse(variables);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Could not parse variables.", exception);
+            }
+        }
+    }
+    #endregion
+}
