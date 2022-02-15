8986beadfcb885f7d54560e2f046c9d7e426dfb0
Leon Granzow <l.granzow@inray.de>
2022-02-15 10:09:42 +0100

    Added class for general static methods

---
+++ UACloudLibraryGraphQLClient/UACloudLibClientLibrary/InternalMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UACloudLibClientLibrary
+{
+    internal static class InternalMethods
+    {
+        public static string LikeComparisonCompatibleString(string value)
+        {
+            string result = "";
+            if(value.StartsWith("%") && value.EndsWith("%"))
+            {
+                result = value;
+            }
+            else
+            {
+                result = string.Format("%" + value + "%");
+            }
+            return result;
+        }
+    }
+}
