1bdd7334095b1bcaccbd6a312c0370aa8ca0dd48
Leon Granzow <l.granzow@inray.de>
2022-02-15 10:13:27 +0100

    Adjusted interface class

--- UACloudLibraryGraphQLClient/UACloudLibClientLibrary/WhereExpression/IWhereExpression.cs
+++ UACloudLibraryGraphQLClient/UACloudLibClientLibrary/WhereExpression/IWhereExpression.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace UACloudLibClientLibrary
@@ -14,19 +15,15 @@ namespace UACloudLibClientLibrary
         Contains,
         StartsWith,
         EndsWith,
-        //In,
         Like
     }
-
     public interface IWhereExpression<T> where T : Enum
     {
-        public T Path { get; set; }
-        public string Value { get; set; }
-        public ComparisonType Comparison { get; set; }
+        public string Expression { get; }
         /// <summary>
         /// Returns a string that is formatted so that it can be used in a where expression in a query
         /// </summary>
         /// <returns>A String formatted like this: {path: "---", comparison: ---, value: "---"}</returns>
-        public string GetExpression();
+        public bool SetExpression(T path, string value, ComparisonType comparison, [Optional]bool connector);
     }
 }
