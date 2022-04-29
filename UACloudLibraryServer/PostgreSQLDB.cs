b56a2703dd2ebc7f64127019d19d8dff8214de92
Markus Horstmann <markushorstmann@hotmail.com>
2022-04-29 11:51:25 -0700

    Remove temporary logging

--- UACloudLibraryServer/PostgreSQLDB.cs
+++ UACloudLibraryServer/PostgreSQLDB.cs
@@ -43,32 +43,22 @@ namespace UACloudLibrary
         private NpgsqlConnection _connection = null;
         private readonly ILogger _logger;
 
-        public static string CreateConnectionString(IConfiguration configuration, ILogger logger = null)
+        public static string CreateConnectionString(IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("CloudLibraryPostgreSQL");
             if (string.IsNullOrEmpty(connectionString))
             {
-                logger?.LogInformation("No connection string in config. Using environment variables.");
-                connectionString = CreateConnectionStringFromEnvironment(logger);
-                // Temporary for troubleshooting
-                if (logger == null)
-                {
-                    throw new ArgumentException("No connection string in config.Using environment variables.");
-                }
+                connectionString = CreateConnectionStringFromEnvironment();
             }
             return connectionString;
         }
 
-        private static string CreateConnectionStringFromEnvironment(ILogger logger)
+        private static string CreateConnectionStringFromEnvironment()
         {
             // Obtain connection string information from the environment
             string Host = Environment.GetEnvironmentVariable("PostgreSQLEndpoint");
             string User = Environment.GetEnvironmentVariable("PostgreSQLUsername");
             string Password = Environment.GetEnvironmentVariable("PostgreSQLPassword");
-            if (string.IsNullOrEmpty(Host))
-            {
-                logger?.LogInformation("Using host {Host} from environment");
-            }
 
             string DBname = "uacloudlib";
             string Port = "5432";
@@ -98,7 +88,7 @@ namespace UACloudLibrary
 
             try
             {
-                _connection = new NpgsqlConnection(CreateConnectionString(configuration, _logger));
+                _connection = new NpgsqlConnection(CreateConnectionString(configuration));
                 _connection.Open();
 
                 foreach (string initCommand in dbInitCommands)
