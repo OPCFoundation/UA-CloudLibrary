d307c9b22304c51a5e24f2fa74abf8aa54c82055
Markus Horstmann <markushorstmann@hotmail.com>
2022-10-17 17:53:05 -0700

    EF: set collation to en_US

--- UACloudLibraryServer/GraphQL/AppDbContext.cs
+++ UACloudLibraryServer/GraphQL/AppDbContext.cs
@@ -88,7 +88,7 @@ namespace Opc.Ua.Cloud.Library
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.HasCollation("en_US.UTF-8", "en_US.UTF-8");
+            builder.HasCollation("en_US", "en_US");
             base.OnModelCreating(builder);
 
             NodeSetModelContext.CreateModel(builder);
