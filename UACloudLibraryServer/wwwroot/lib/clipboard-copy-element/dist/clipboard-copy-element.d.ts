aea7ed1ced083d273271f43c7dc321ee3e0f9f63
Erich Barnstedt <erichb@microsoft.com>
2024-09-24 14:10:20 +0200

    Removed clipboard-copy-element script and button due to several high-severity and unfixed security advisories. A small price to pay for losing the functionality of not having to get users to press Control-C!

--- UACloudLibraryServer/wwwroot/lib/clipboard-copy-element/dist/clipboard-copy-element.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ClipboardCopyElement extends HTMLElement {
-    static define(tag?: string, registry?: CustomElementRegistry): typeof ClipboardCopyElement;
-    constructor();
-    connectedCallback(): void;
-    get value(): string;
-    set value(text: string);
-}
