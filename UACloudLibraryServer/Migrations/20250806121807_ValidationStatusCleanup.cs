478d3ee5440a898151c7b538a372d28e8e282802
barnstee <erichb@microsoft.com>
2025-08-07 00:27:58 +0200

    fix encoding

--- UACloudLibraryServer/Migrations/20250806121807_ValidationStatusCleanup.cs
+++ UACloudLibraryServer/Migrations/20250806121807_ValidationStatusCleanup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
--- UACloudLibraryServer/Migrations/20250806141923_FixValidationStatus.cs
+++ UACloudLibraryServer/Migrations/20250806141923_FixValidationStatus.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
