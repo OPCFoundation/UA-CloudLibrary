f58f869334640e5a69c40d44b4757c46edb4305e
Erich Barnstedt <erichb@microsoft.com>
2025-08-19 10:06:21 +0200

    Add save updated nodeset values functionality.

--- UACloudLibraryServer/Controllers/BrowserController.cs
+++ UACloudLibraryServer/Controllers/BrowserController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Opc.Ua.Cloud.Library;
 
 namespace AdminShell
 {
@@ -11,17 +12,20 @@ namespace AdminShell
     public class BrowserController : Controller
     {
         private readonly UAClient _client;
+        private readonly DbFileStorage _storage;
 
-        public BrowserController(UAClient client)
+        public BrowserController(UAClient client, DbFileStorage storage)
         {
             _client = client;
+            _storage = storage;
         }
 
-        public ActionResult Index(string nodesetIdentifier, string nodesetName)
+        public ActionResult Index(string nodesetIdentifier, string nodesetName, string userName)
         {
             return View("Index", new BrowserModel() {
                 NodesetIdentifier = nodesetIdentifier,
-                NodesetName = nodesetName
+                NodesetName = nodesetName,
+                UserName = userName
             });
         }
 
@@ -31,6 +35,41 @@ namespace AdminShell
             Dictionary<string, string> results = await _client.BrowseVariableNodesResursivelyAsync(model.NodesetIdentifier, null).ConfigureAwait(false);
 
             return File(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(results)), "text/json", "nodevalues.json");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Save(BrowserModel model)
+        {
+            if (User.Identity.Name == model.UserName)
+            {
+                DbFiles nodesetXml = await _storage.DownloadFileAsync(model.NodesetIdentifier).ConfigureAwait(false);
+
+                Dictionary<string, string> results = await _client.BrowseVariableNodesResursivelyAsync(model.NodesetIdentifier, null).ConfigureAwait(false);
+                nodesetXml.Values = JsonConvert.SerializeObject(results);
+
+                string name = await _storage.UploadFileAsync(model.NodesetIdentifier, nodesetXml.Blob, nodesetXml.Values).ConfigureAwait(false);
+                if (name == null)
+                {
+                    return Json(new {
+                        success = false,
+                        message = "Failed to save changes to the nodeset."
+                    });
+                }
+                else
+                {
+                    return Json(new {
+                        success = true,
+                        message = "Save operation successful"
+                    });
+                }
+            }
+            else
+            {
+                return Json(new {
+                    success = false,
+                    message = "You are not authorized to save changes for this nodeset."
+                });
+            }
         }
     }
 }
