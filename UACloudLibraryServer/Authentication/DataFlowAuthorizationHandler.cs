6f64895a1bec4807723434456c8d2a977b8fd0fe
Erich Barnstedt <erichb@microsoft.com>
2025-08-21 15:47:27 +0200

    Dataplane Signaling API, initial checkin.

---
+++ UACloudLibraryServer/Authentication/DataFlowAuthorizationHandler.cs
@@ -0,0 +1,46 @@
+
+namespace Opc.Ua.Cloud.Library.Authentication
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using DataPlane.Sdk.Api.Authorization;
+    using DataPlane.Sdk.Api.Authorization.DataFlows;
+    using DataPlane.Sdk.Core.Domain.Interfaces;
+    using Microsoft.AspNetCore.Authorization;
+
+    public class DataFlowAuthorizationHandler(IDataPlaneStore store)
+    : AuthorizationHandler<DataFlowRequirement, ResourceTuple>
+    {
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            DataFlowRequirement requirement, ResourceTuple resource)
+        {
+            var (participantContextId, dataFlowId) = resource;
+
+            // Verify that the participant context ID (from the request) matches the user ID in the claims
+            var principal = context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            if (participantContextId != principal)
+            {
+                context.Fail();
+                return;
+            }
+
+            // can happen on endpoints that don't target a specific dataflow (e.g. "start")
+            if (dataFlowId == null)
+            {
+                context.Succeed(requirement);
+                return;
+            }
+
+            var dataFlow = await store.FindByIdAsync(dataFlowId);
+            //if null, then it's not a permission problem, but a data-flow-not-found problem
+            if (dataFlow == null || dataFlow.ParticipantId == participantContextId)
+            {
+                context.Succeed(requirement);
+                return;
+            }
+
+            // user does not own dataflow
+            context.Fail();
+        }
+    }
+}
