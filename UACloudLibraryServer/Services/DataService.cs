a26b6ac20905210ffb323ce3a4cfbe3b7121311c
Erich Barnstedt <erichb@microsoft.com>
2025-09-11 21:00:39 +0200

    whitespace fixes.

--- UACloudLibraryServer/Services/DataService.cs
+++ UACloudLibraryServer/Services/DataService.cs
@@ -21,19 +21,20 @@ public class DataService(IDataPlaneStore dataFlowStore) : IDataService
 
     public async Task<DataFlow> CreatePublicEndpoint(DataFlow dataFlow)
     {
-        var id = dataFlow.Id; //todo: should this be the DataAddress ID or even randomly generated?
-        var apiToken = Guid.NewGuid().ToString();
+        string id = dataFlow.Id; //todo: should this be the DataAddress ID or even randomly generated?
+
+        string apiToken = Guid.NewGuid().ToString();
 
         dataFlow.State = DataFlowState.Started;
-        dataFlow.Destination = new DataAddress("HttpData")
-        {
-            Properties =
-            {
+        dataFlow.Destination = new DataAddress("HttpData") {
+            Properties = {
                 ["url"] = $"http://localhost:8080/api/v1/public/{id}",
                 ["token"] = apiToken
             }
         };
+
         await dataFlowStore.UpsertAsync(dataFlow, true);
+
         return dataFlow;
     }
 }
