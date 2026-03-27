157f7b070ff8e54db9d78300f3e542e92d0b34d7
Erich Barnstedt <erichb@microsoft.com>
2026-03-27 16:08:38 +0100

    Revised mapping from AAS to DPP

---
+++ UACloudLibraryServer/Models/File.cs
@@ -0,0 +1,33 @@
+
+namespace AdminShell
+{
+    using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+    using System.Runtime.Serialization;
+    using System.Xml.Serialization;
+
+    [DataContract]
+    public class File : SubmodelElement
+    {
+        [Required]
+        [DataMember(Name = "contentType")]
+        [XmlElement(ElementName = "mimeType")]
+        public string MimeType { get; set; }
+
+        [DataMember(Name = "value")]
+        [XmlElement(ElementName = "value")]
+        public string Value { get; set; }
+
+        public File(SubmodelElement src)
+        {
+            if (!(src is File file))
+            {
+                return;
+            }
+
+            MimeType = file.MimeType;
+            Value = file.Value;
+        }
+    }
+}
+
